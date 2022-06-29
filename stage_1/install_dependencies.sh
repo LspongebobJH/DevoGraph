7089e1ba2397eeeb00d9e9233db57e360ad1543e
watarungurunnn <seikyozasu@gmail.com>
2022-06-29 21:09:07 +0900

    try stage_1

---
+++ stage_1/install_dependencies.sh
@@ -0,0 +1,3 @@
+pip install dgl jupyter git+https://github.com/DevoLearn/devolearn
+pip uninstall torch torchvision torchaudio
+conda install torchvision==0.8.0 torchaudio==0.7.0 pytorch==1.7.0 -c pytorch
